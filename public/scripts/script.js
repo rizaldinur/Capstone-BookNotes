1ceaba3cded22b975041eeba0a215806684e982b
rizaldinur <rnur71@gmail.com>
2024-08-22 12:05:24 +0700

    initial script for front end

---
+++ public/scripts/script.js
@@ -0,0 +1,57 @@
+function showSearchBar() {
+  $("#togglerIcon").toggleClass("fa-times");
+  $(".search-column").slideToggle();
+}
+
+$(document).ready(function () {
+  const dataContainer = document.getElementById("book-container");
+  const cards = Array.from(
+    dataContainer.getElementsByClassName("card-container")
+  );
+  console.log(cards[0].outerHTML);
+
+  const itemsPerPage = 6;
+  let currentPage = 1;
+  const totalPages = Math.ceil(cards.length / itemsPerPage);
+
+  //display content
+  function displayContent(page) {
+    $("#book-container").empty();
+    const start = (page - 1) * itemsPerPage;
+    const end = Math.min(start + itemsPerPage, cards.length);
+    // cards.forEach((card, index) => {
+    //   if (index >= start && index < end) {
+    //     card.style.display = "block";
+    //   } else {
+    //     card.style.display = "none";
+    //   }
+    // });
+    for (let i = start; i < end; i++) {
+      $("#book-container").append(cards[i].outerHTML);
+    }
+
+    // Update page indicator
+    $("#pageIndicator").text(`Page ${page} of ${totalPages}`);
+
+    // Disable/enable buttons based on page
+    $("#prevPage").prop("disabled", page === 1);
+    $("#nextPage").prop("disabled", page === totalPages);
+  }
+
+  $("#prevPage").click(function () {
+    if (currentPage > 1) {
+      currentPage--;
+      displayContent(currentPage);
+    }
+  });
+
+  $("#nextPage").click(function () {
+    if (currentPage < totalPages) {
+      currentPage++;
+      displayContent(currentPage);
+    }
+  });
+
+  // Initial display
+  displayContent(currentPage);
+});
