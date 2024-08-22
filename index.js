ab829191ec875d85d1d0ef5c0d1245c43dc842e2
rizaldinur <rnur71@gmail.com>
2024-08-22 19:59:03 +0700

    initial setup

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+// set up db connection
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "surabaya12",
+  port: 5432,
+});
+
+db.connect();
+
+// read data from db
+db.query("SELECT * from capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
+
+const app = express();
+const port = 3000;
+
+let items = [];
+
+let totalCorrect = 0;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentQuestion = {};
+
+// GET home page
+app.get("/", async (req, res) => {});
+
+// POST a new post
+app.post("/submit", (req, res) => {});
+
+async function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+
+  currentQuestion = randomCountry;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
