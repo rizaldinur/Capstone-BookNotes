916e8f5c559bea394838ba43d7ac326cad5e1406
rizaldinur <rnur71@gmail.com>
2024-09-01 19:04:59 +0700

    fix dbconfig (2)

--- DBConfig.js
+++ DBConfig.js
@@ -2,10 +2,9 @@ import pkg from "pg";
 
 const { Pool } = pkg;
 
-const itemsPool = new Pool({
+export const itemsPool = new Pool({
   connectionString: process.env.DBConfigLink,
   ssl: {
     rejectUnauthorized: false,
   },
 });
-module.exports = itemsPool;
